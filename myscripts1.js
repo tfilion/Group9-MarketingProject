65405713b5eaa7894563e788c8dae6fabb55723a
tfilion <43012892+tfilion@users.noreply.github.com>
2018-12-13 18:45:47 -0500

    adjusting images

--- myscripts1.js
+++ myscripts1.js
@@ -11,3 +11,16 @@ function mUp(obj) {
     obj.style.border="3px solid green"
     obj.innerHTML="Order now!";
   }
+  window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("myBtn").style.display = "block";
+  } else {
+    document.getElementById("myBtn").style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
