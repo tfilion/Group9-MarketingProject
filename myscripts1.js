d074c189d07655151de9798c8cf086a840c8f6d9
tfilion <43012892+tfilion@users.noreply.github.com>
2018-12-13 12:40:57 -0500

    javascript done

--- myscripts1.js
+++ myscripts1.js
@@ -1,1 +1,13 @@
-function
+
+function mDown(obj) {
+    obj.style.backgroundColor = "green";
+    obj.style.color="white";
+    obj.innerHTML = "Let go!";
+  }
+  
+function mUp(obj) {
+    obj.style.backgroundColor="white";
+    obj.style.color="green";
+    obj.style.border="3px solid green"
+    obj.innerHTML="Order now!";
+  }
